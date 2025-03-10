b2e506b1f04bf49807a7709367db9884246e2e63
pradhumn1507 <pradhumn1508@gmail.com>
2025-03-10 12:40:08 +0530

    update

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,4 +8,20 @@ export default defineConfig({
       '/api': 'http://localhost:5000',
     },
   },
+  optimizeDeps: {
+    exclude: [
+      "chunk-64HT3LH6.js",
+      "chunk-5WN4RLNU.js"
+    ]
+  },
+  build: {
+    // Optional: Prevent Vite from stopping on warnings or TypeScript errors
+    rollupOptions: {
+      onwarn(warning, warn) {
+        // Ignore TS warnings
+        if (warning.code === 'TS') return;
+        warn(warning);
+      },
+    },
+  },
 });
